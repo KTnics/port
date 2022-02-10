e2ac9e82df282d674474fb937e31a839c9dedeec
KTNICK <85989135+KTnic@users.noreply.github.com>
2022-02-10 13:58:31 +0530

    s

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 MEDIA_URL = "/public/"
-MEDIA_ROOT = BASE_DIR / "Medias"
+MEDIA_ROOT = BASE_DIR / "medias"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
